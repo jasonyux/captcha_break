b4c127fe174d765411fad1c51e10b697e7c1b803
Xiao Yu <jasonxyu@tencent.com>
2021-05-27 15:17:08 +0800

    added result statistics

--- Winpy/recognize.py
+++ Winpy/recognize.py
@@ -1,9 +1,10 @@
-import random
+import random, re
 import torch, os, string
 from PIL import Image
 from torchvision.transforms.functional import to_tensor
 
 LABEL_LENGTH = 4
+FILE_LABEL_PATTERN = r'([0-9a-zA-Z]+)_([0-9]+)(\.png)'
 CHARACTERS = string.digits + string.ascii_uppercase + string.ascii_lowercase
 GPU_MODE = torch.cuda.is_available()
 
@@ -32,8 +33,25 @@ def predict_image(model, image_path):
     else:
         output = model(image.unsqueeze(0).cuda())    
     output_argmax = output.detach().permute(1, 0, 2).argmax(dim=-1)
-    print('pred:', decode(output_argmax[0]))
-    return
+    result = decode(output_argmax[0])
+    print(f'pred:{result}')
+    return result
+
+
+"""
+Expects label in of the image in the format specified in @FILE_LABEL_PATTERN
+"""
+def get_label(actual_img_path):
+    file = os.path.split(actual_img_path)[-1]
+    matchObj = re.match(FILE_LABEL_PATTERN, file)
+    return matchObj.group(1)
+
+
+def check_result(pred:str, actual_img_path):
+    label = get_label(actual_img_path)
+    print(f'actual={label}')
+    return label==pred
+
 
 if __name__ == "__main__":
     if GPU_MODE==False:
@@ -41,10 +59,18 @@ if __name__ == "__main__":
     else:
         model = torch.load('ctc3.pth')
     model.eval()
-    dir_path = 'real'
+    dir_path = '/root/ZhihuScraper/captchas/labeled'
     dir = os.listdir(dir_path)
     random.shuffle(dir)
-    for file in dir[:10]:
+    fully_correct = 0.0
+    partial_correct = 0.0
+    for file in dir[:]:
         img_path = os.path.join(dir_path, file)
         # predict_image(model, img_path)
-        predict_image(model, img_path)
+        pred = predict_image(model, img_path)
+        actual = get_label(img_path)
+        if pred == actual:
+            fully_correct += 1.
+        if pred.lower() == actual.lower():
+            partial_correct += 1.
+    print(f"full success rate={fully_correct/len(dir)} partial success rate={partial_correct/len(dir)}")
