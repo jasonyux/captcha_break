9f9509c4396bd3f44016f15d2c2d3e63837284d0
Xiao Yu <jasonxyu@tencent.com>
2021-05-28 18:57:46 +0800

    initialized self-training model

--- Winpy/CaptchaDataset.py
+++ Winpy/CaptchaDataset.py
@@ -10,12 +10,13 @@ import random, os, re
 import numpy as np
 from collections import OrderedDict
 from PIL import Image
+from pathlib import Path
 
 IMAGE_DATA_DIR = "real/labeled"
 FILE_LABEL_PATTERN = r'([0-9a-zA-Z]+)_([0-9]+)(\.png)'
 
 class CaptchaDataset(Dataset):
-    def __init__(self, characters, length, width, height, input_length, label_length):
+    def __init__(self, characters, length, width, height, input_length, label_length, latest_first=False):
         super(CaptchaDataset, self).__init__()
         self.characters = characters
         self.length = length
@@ -25,6 +26,7 @@ class CaptchaDataset(Dataset):
         self.label_length = label_length
         self.n_class = len(characters)
         self.generator = ImageCaptcha(width=width, height=height)
+        self.latest_first = latest_first
 
     def __len__(self):
         return self.length
@@ -41,12 +43,36 @@ class CaptchaDataset(Dataset):
 
     
     def __getimage(self, dir=IMAGE_DATA_DIR):
-        files = os.listdir(dir)
-        file = random.choice(files)
+        file = None
+        if self.latest_first == True:
+            # this will put the EARLIEST in front
+            # smallest first distribution, with last one being 1.0
+            files = sorted(Path(dir).iterdir(), key=os.path.getmtime)
+            distri = self.linear_distribution(len(files))
+            for pox, curr_file in enumerate(files):
+                if random.random() < distri[pox]:
+                    file = curr_file
+                    break
+            if file is None:
+                # not possible
+                return
+        else:
+            files = os.listdir(dir)
+            random.shuffle(files)
+            file = random.choice(files)
+
+        file = os.path.split(file)[-1]
         matchObj = re.match(FILE_LABEL_PATTERN, file)
         label = matchObj.group(1)
         image = Image.open(os.path.join(dir, file))
         if image.mode != 'RGB': # need 3 channel RGB
 	        image = image.convert('RGB')
         return label, image
+
+    def linear_distribution(self, length):
+        if length == 0:
+            # error
+            return None
+        result = [(i+1.0)/length for i in range(length)]
+        return result
 
--- Winpy/main.py
+++ Winpy/main.py
@@ -1,4 +1,4 @@
-import torch
+import torch, os, time, logging
 import torch.nn as nn
 import torch.nn.functional as F
 from torch.utils.data import Dataset, DataLoader
@@ -16,6 +16,8 @@ from CaptchaDataset import CaptchaDataset
 from Model import Model
 from Utilss import train
 from Utilss import valid
+
+IMAGE_DATA_DIR = "real/labeled"
 
 model = None
 characters = None
@@ -39,14 +41,14 @@ def init():
     print(''.join([characters[x] for x in target]), input_length, label_length)
     to_pil_image(image)
 
-
+    """
     batch_size = 70
     train_set = CaptchaDataset(characters, 1000 * batch_size, width, height, n_input_length, n_len)
     valid_set = CaptchaDataset(characters, 100 * batch_size, width, height, n_input_length, n_len)
     train_loader = DataLoader(train_set, batch_size=batch_size, num_workers=2)
     valid_loader = DataLoader(valid_set, batch_size=batch_size, num_workers=2)
-
-
+    """
+    train_loader, valid_loader = config_loader(batch_size = 70)
 
     model = Model(n_classes, input_shape=(3, height, width))
     inputs = torch.zeros((32, 3, height, width))
@@ -56,6 +58,13 @@ def init():
     model = Model(n_classes, input_shape=(3, height, width))
     model = model.cuda()
     print(model)
+
+def config_loader(batch_size=10, width=150, height=60, n_len=4, n_input_length = 9, latest_first=False):
+    train_set = CaptchaDataset(characters, 20 * batch_size, width, height, n_input_length, n_len, latest_first=latest_first)
+    valid_set = CaptchaDataset(characters, 2 * batch_size, width, height, n_input_length, n_len, latest_first=latest_first)
+    train_loader = DataLoader(train_set, batch_size=batch_size, num_workers=2)
+    valid_loader = DataLoader(valid_set, batch_size=batch_size, num_workers=2)
+    return train_loader, valid_loader
 
 def decode(sequence):
     a = ''.join([characters[x] for x in sequence])
@@ -77,21 +86,91 @@ def calc_acc(target, output):
     return a.mean()
 
 
+def save_ckp(epochs, optimizer, model_ckp_path, model_path):
+    global model
+    torch.save({
+        'epoch': epochs,
+        'model_state_dict': model.state_dict(),
+        'optimizer_state_dict': optimizer.state_dict(),
+        }, 
+        model_ckp_path
+    )
+    torch.save(model, model_path)
+    return
+
+def train_model():
+    total_epochs = 0
+    optimizer = torch.optim.Adam(model.parameters(), 1e-3, amsgrad=True)
+    # epochs = 6
+    """
+    epochs = 2
+    for epoch in range(1, epochs + 1):
+        train(model, optimizer, epoch, train_loader,characters)
+        valid(model, optimizer, epoch, valid_loader,characters)
+    total_epochs += epochs
+    """
+
+    optimizer = torch.optim.Adam(model.parameters(), 1e-4, amsgrad=True)
+    # epochs = 3
+    epochs = 1
+    for epoch in range(1, epochs + 1):
+        train(model, optimizer, epoch, train_loader,characters)
+        valid(model, optimizer, epoch, valid_loader,characters)
+    total_epochs += epochs
+    return total_epochs, optimizer
+
+
+def train_when_new(new_num=100, dir=IMAGE_DATA_DIR, model_ckp_path='ctc3_test_ckp.pth', model_path='ctc3_test.pth'):
+    old_length = 0
+    while(True):
+        length = len(os.listdir(dir))
+        diff = length - old_length
+        if(diff < new_num):
+            logging.info(f'only {diff} new files')
+            # checks every minute
+            time.sleep(60)
+            # up
+            continue
+        else:
+            logging.info('training new')
+            __train_new(model_ckp_path, model_path)
+            # update
+            old_length = length
+    return
+
+
+def __train_new(model_ckp_path, model_path):
+    config_loader(latest_first=True)
+    total_epochs, optimizer = train_model()
+    save_ckp(total_epochs, optimizer, model_ckp_path, model_path)
+    return
+
 
 if __name__=='__main__':
+    logging.basicConfig(level=logging.INFO)
     init()
+    model_ckp_path='ctc3_test_ckp.pth'
+    model_path='ctc3_test.pth'
+    total_epochs, optimizer = train_model()
+    save_ckp(total_epochs, optimizer, model_ckp_path, model_path)
+    # after the first time, new updates should have latest_first=True
+    train_when_new(model_ckp_path=model_ckp_path, model_path=model_path)
 
+    """
     optimizer = torch.optim.Adam(model.parameters(), 1e-3, amsgrad=True)
-    epochs = 6
+    # epochs = 6
+    epochs = 2
     for epoch in range(1, epochs + 1):
         train(model, optimizer, epoch, train_loader,characters)
         valid(model, optimizer, epoch, valid_loader,characters)
 
     optimizer = torch.optim.Adam(model.parameters(), 1e-4, amsgrad=True)
-    epochs = 3
+    # epochs = 3
+    epochs = 1
     for epoch in range(1, epochs + 1):
         train(model, optimizer, epoch, train_loader,characters)
         valid(model, optimizer, epoch, valid_loader,characters)
+    
     
     
     model.eval()
@@ -111,4 +190,5 @@ if __name__=='__main__':
 
 
     torch.save(model, 'ctc3_test.pth')
+    """
     
